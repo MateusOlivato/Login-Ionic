566289d456658673433453c398d514d154aa42ed
MateusOlivato <50343592+MateusOlivato@users.noreply.github.com>
2020-05-14 14:53:20 -0300

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+//importações necessárias
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+
+/*
+Para Funcionar os formulários, precisamos importar
+o módulo ReactiveFormsModule no arquivo .module.ts
+*/
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public formLogin: FormGroup;
+
+  public mensagens_validacao = {
+    email: [
+    {tipo: 'required', mensagem: 'O campo E-mail é obrigatório'},
+    {tipo: 'email', mensagem: 'Email Inválido'}
+    ],
+
+    senha: [
+      {tipo: 'required', mensagem: 'É obrigatório digitar a senha'},
+      {tipo: 'minlenght', mensagem: 'A senha deve ter pelo menos 6 caractéres.'},
+      {tipo: 'maxlengh', mensagem: 'A senha deve ter no máximo 8 caractéres'}
+    ],
+
+
+  }
+  constructor(public formBuilder: FormBuilder, public alertController: AlertController, public router: Router) {
+
+    //Monta o formulário
+    this.formLogin = formBuilder.group({
+      // Declara os campos do formulario
+      email:['', Validators.compose([Validators.email, Validators.required])],
+      senha:['', Validators.compose([Validators.minLength(6), Validators.maxLength(8), Validators.required])]
+    });
+  }
+
+  public login(){
+    if(this.formLogin.valid){
+      let email = this.formLogin.value.email;
+      let senha = this.formLogin.value.senha;
+
+      if(email == 'admin@admin.com' && senha == "123456"){
+        this.router.navigateByUrl('painel-usuario');
+      }else{
+        this.alertUserInvalid()
+      }
+
+    }else{
+      this,this.alertFormInvalid();
+    }
+
+
+  }
+
+  
+
+  async alertFormInvalid(){
+    const alert = await this.alertController.create({
+      header: 'ERRO!!!',
+      message: 'Formulário inválido, confira os dados!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+  }
+
+    async alertUserInvalid(){
+      const alert = await this.alertController.create({
+        header: 'ERRO!!!',
+        message: 'E-mail/Senha inválidos, confira os dados!',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+  }
+
+}
